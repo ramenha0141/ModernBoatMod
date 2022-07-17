c2f1bf1b688372fc08cc59f0710408c95ad1c7ef
ramenha0141 <ramenhaoishii@gmail.com>
2022-07-17 10:50:38 +0900

    Initial commit

---
+++ src/main/java/net/ramen/modernboatmod/ModernBoatMod.java
@@ -0,0 +1,15 @@
+package net.ramen.modernboatmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModernBoatMod implements ModInitializer {
+	public static final String MOD_ID = "modernboatmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ramen/modernboatmod/ModernBoatModClient.java
@@ -0,0 +1,10 @@
+package net.ramen.modernboatmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModernBoatModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
